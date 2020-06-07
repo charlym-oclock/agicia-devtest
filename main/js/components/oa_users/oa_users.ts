1542e9bbe541b472ec2b333d0917802d60ca6939
Etudiant O'clock <etudiant@teleporteur.oclock>
2020-06-07 16:28:59 +0200

    users list now imported from .ts file

--- main/js/components/oa_users/oa_users.ts
+++ main/js/components/oa_users/oa_users.ts
@@ -2,6 +2,15 @@ import * as angular from "angular"
 
 class UsersViewController{
 
+  public users: any[] = [
+    {name : 'charly', age : 28, sex : 'male'},
+    {name : 'amelie', age : 24, sex : 'female'},
+    {name : 'arthur', age : 45, sex : 'male'},
+    {name : 'annie', age : 69, sex : 'female'},
+    {name : 'nicolas', age : 12, sex : 'male'},
+    {name : 'clementine', age : 11, sex : 'female'},
+  ]
+
   public constructor(){}
 
   public $onInit(): void{
