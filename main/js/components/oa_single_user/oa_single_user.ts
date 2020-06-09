ca5d23beb2fca2ac384c7d919d10ff4e075cec57
Luinilbleue <luinilbleue@gmail.com>
2020-06-09 12:34:29 +0200

    routerOnActivate to retrieve user name from URL

--- main/js/components/oa_single_user/oa_single_user.ts
+++ main/js/components/oa_single_user/oa_single_user.ts
@@ -2,11 +2,42 @@ import * as angular from "angular"
 
 class SingleUserViewController{
 
+  public constructor(private $http: angular.IHttpService, $scope: any) {
+    
   // je fais ma requete HTTP pour récupérer mes données depuis l'API
-  public constructor(private $http: angular.IHttpService, $scope: any, $location: any){
-    // je recupere le nom d'utilisateur via mon URL
-    var userName = "j'arrive bien sur la page d'une personne";
+
+  // je creer une variable avec toutes mes infos pour passer ma requete
+    var request = {
+      // la methode de la requete
+      method: 'GET',
+      // l'url a laquelle j'adresse ma requete
+      url: 'http://127.0.0.1:5000/users/',
+      // j'ajoute un parametre pour aller chercher l'ID de l'utilisateur
+      // params: userName,
+      // et les headers pour m'eviter les problemes de CORS
+      headers: {
+        'Access-Control-Allow-Origin': 'http://localhost:8000'
+      },
+    }
+
+    $http(request).then(function success(response) {
+        // je stocke les données récupérées dans une variable que je pourrai transmettre a ma vue
+      $scope.user = response.data;
+      // je verifie en console ce que me renvoie la requete
+      console.log($scope.user);
+    }, function error(response){
+      // en cas d'echec, je transmet le status d'erreur a ma vue
+      $scope.user = response.statusText;
+      // je verifie en console ce que me renvoie la requete
+      console.log($scope.user);
+    })
+  }
+
+  public $routerOnActivate = function(next: any, previous: any): void {
+  // recuperer le nom dans l'URL
+    var userName = next.params.id;
     console.log(userName)
+    return userName;
   }
 
   public $onInit(): void{
