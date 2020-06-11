06136b1b2e8bdebc3dd68261bf2522535cbd55e3
Luinilbleue <luinilbleue@gmail.com>
2020-06-11 14:40:59 +0200

    search field now triggers http request to get user data

--- main/js/components/oa_users_list/oa_users_list.ts
+++ main/js/components/oa_users_list/oa_users_list.ts
@@ -34,6 +34,32 @@ class UsersListViewController {
     //   })
   }
 
+
+  // je creer une methode qui va aller chercher mes infos sur l'API
+  public getData(name: any): void {
+    // this.userName = name;
+    // console.log("fonction getData appelée")
+    // je fais ma requete HTTP pour récupérer mes données depuis l'API
+    // je commence par creer une variable avec toutes mes infos pour passer ma requete
+    var request = {
+      // la methode de la requete
+      method: 'GET',
+      // l'url a laquelle j'adresse ma requete
+      url: `http://127.0.0.1:5000/users/${name}`,
+      // et les headers pour m'eviter les problemes de CORS
+      headers: {
+        'Access-Control-Allow-Origin': 'http://localhost:8000'
+      },
+    }
+
+    this.$http(request).then((response): void => {
+      // je stocke les données récupérées dans une variable que je pourrai transmettre a ma vue
+      this.usersList = response.data;
+      // je verifie en console ce que me renvoie la requete
+      console.log(this.usersList);
+    })
+  }
+
   public $onInit(): void {
   }
 
